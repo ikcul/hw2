2e658e6fbbf17b5c31b02d575c406c0ceacd191a
Daniel <simdaniel06@gmail.com>
2025-10-05 01:02:35 -0700

    working out add to cart

--- clothing.cpp
+++ clothing.cpp
@@ -35,12 +35,11 @@ bool Clothing::isMatch(std::vector<std::string>& searchTerms) const{
 /**
  * Returns a string to display the product info for hits of the search
  */
-std::string Clothing::displayString() const {
-    std::stringstream ss;
-    ss << getName() << "\n"
-       << "Size: " << size_ << " Brand: " << brand_ << "\n"
-       << std::fixed << std::setprecision(2) << getPrice() << " " << getQty() << " left.";
-    return ss.str();
+std::string Clothing::displayString() const{
+    std::string display = name_ + "\n" + 
+                "Size: " + size_ + " Brand: " + brand_ + "\n" + 
+                to_string(price_) + " " + to_string(qty_) + " left.";
+    return display;
 }
 
 /**
